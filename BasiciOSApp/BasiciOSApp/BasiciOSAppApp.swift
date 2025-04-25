8d7a695b38113ed76b5a54a9368047389923af04
DedeepyaRSalla <146378612+DedeepyaRSalla@users.noreply.github.com>
2025-04-24 21:04:04 -0500

    Added new ios app

---
+++ BasiciOSApp/BasiciOSApp/BasiciOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasiciOSAppApp.swift
+//  BasiciOSApp
+//
+//  Created by Dedeepya Salla on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BasiciOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasiciOSApp/BasiciOSApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasiciOSApp
+//
+//  Created by Dedeepya Salla on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
